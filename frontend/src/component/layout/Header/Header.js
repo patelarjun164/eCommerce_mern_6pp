e92852c6ead79a45e92609e28f5c31defa093f1f
Arjun Patel <hackmech007@gmail.com>
2024-06-29 16:08:22 +0530

    tried new aproach but not working

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,64 +1,49 @@
 import React, { useState } from 'react';
-import { FaUserCircle, FaShoppingCart, FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
+  const showSidebar = () => {
+    setSidebarVisible(true);
+  };
+
+  const hideSidebar = () => {
+    setSidebarVisible(false);
   };
 
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          Ecommerce
-        </Link>
-
-        <div className="menu-icon" onClick={toggleMenu}>
-          {isOpen ? <FaTimes /> : <FaBars />}
-        </div>
-
-        <ul className={isOpen ? 'nav-menu active' : 'nav-menu'}>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/products" className="nav-links">
-              Products
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-links">
-              Contact
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-links">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <input type="text" className="search-bar" placeholder="Search..." />
-          </li>
-          <li className="nav-item">
-            <Link to="/account" className="nav-links">
-              <FaUserCircle className="nav-icon" />
-              <span className="nav-text">Account</span>
-            </Link>
-          </li>
-        </ul>
-          <div className="nav-item">
-            <Link to="/cart" className="nav-links">
-              <FaShoppingCart className="nav-icon" />
-              <span className="nav-text">Shopping Cart</span>
-            </Link>
-          </div>
-      </div>
+    <nav>
+      <ul className={`sidebar ${sidebarVisible ? 'visible' : ''}`}>
+        <li onClick={hideSidebar}>
+          <Link to="#">
+            <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 96 960 960" width="26">
+              <path d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" />
+            </svg>
+          </Link>
+        </li>
+        <li><Link to="#">Blog</Link></li>
+        <li><Link to="#">Products</Link></li>
+        <li><Link to="#">About</Link></li>
+        <li><Link to="#">Forum</Link></li>
+        <li><Link to="#">Login</Link></li>
+      </ul>
+      <ul>
+        <li><Link to="#">Coding2go</Link></li>
+        <li className="hideOnMobile"><Link to="#">Blog</Link></li>
+        <li className="hideOnMobile"><Link to="#">Products</Link></li>
+        <li className="hideOnMobile"><Link to="#">About</Link></li>
+        <li className="hideOnMobile"><Link to="#">Forum</Link></li>
+        <li className="hideOnMobile"><Link to="#">Login</Link></li>
+        <li className="menu-button" onClick={showSidebar}>
+          <Link to="#">
+            <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 96 960 960" width="26">
+              <path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
+            </svg>
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
