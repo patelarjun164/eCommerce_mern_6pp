7b496d07a8c45003b40cd7170602bad23e2fc23a
Arjun Patel <hackmech007@gmail.com>
2024-07-08 01:29:07 +0530

    updated cors policy

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,23 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
+const cors = require('cors');
+// Define your CORS options
+const corsOptions = {
+    origin: 'https://shoppynexxa.vercel.app/', // Allow only this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific methods
+    optionsSuccessStatus: 204, // Response status for successful OPTIONS requests
+};
 
+// Use the cors middleware with the defined options
+app.use(cors(corsOptions));
 
 //config
-dotenv.config({path:"config/config.env"});
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    dotenv.config({ path: "config/config.env" });
+}
 
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,11 +34,11 @@ const order = require('./routes/orderRoute');
 const payment = require('./routes/paymentRoute');
 const auth = require('./routes/authRoute');
 
-app.use("/api/v1",product);
-app.use("/api/v1",user);
-app.use("/api/v1",order);
-app.use("/api/v1",payment);
-app.use("/api/v1",auth);
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+app.use("/api/v1", auth);
 
 //Middleware for errors
 app.use(errorMiddleware);
